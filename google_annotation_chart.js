db425ee7f56c241dd69c4e100ee8ffadd22b4ed0
vanne02135 <antti.vanne@iki.fi>
2014-07-13 12:42:45 +0300

    View is set automatically to visible data range

--- google_annotation_chart.js
+++ google_annotation_chart.js
@@ -1,7 +1,6 @@
-
 // see also http://jsfiddle.net/GhEh9/
 
-var chart; 
+var chart;
 
 function drawChart() {
     var data;
@@ -9,15 +8,6 @@ function drawChart() {
     data.addColumn('date', 'Date');
     data.addColumn('number', 'yläkerta temp');
     data.addColumn('string', 'yläkerta annotation');
-
-    data.addRows([
-        [new Date(2314, 2, 15), 25, "semmosta"],
-        [new Date(2314, 2, 16), 28, "tommosta"],
-        [new Date(2314, 2, 17), 10, "oho"]
-
-
-    ]);
-
 
     chart = new google.visualization.AnnotationChart(document.getElementById('chart_div'));
 
@@ -29,45 +19,29 @@ function drawChart() {
 
     $.getJSON('https://api.thingspeak.com/channels/14421/feed.json?callback=?', function (data) {
 
-        // get the data point
-        p = data.field1;
-        if (p) {
-            p = Math.round((p / 1023) * 100);
-            //displayData(p);
-        }
-
         var data2 = new google.visualization.DataTable();
         data2.addColumn('date', 'Date');
         data2.addColumn('number', 'yläkerta temp');
         data2.addColumn('string', 'yläkerta annotation');
 
-
         var a = [];
         var dt = [];
-        for (var i = 0; i < 100; i++) {
-//            alert(data.feeds[i].field1);
+        for (var i = 0; i < data.feeds.length; i++) {
             if (data.feeds[i].field1) {
                 a.push(parseFloat(data.feeds[i].field1));
                 dt.push(data.feeds[i].created_at);
-                
+
             }
         }
 
         for (var i = 0; i < dt.length; i++) {
             data2.addRow([new Date(dt[i]), a[i], undefined]);
         }
-        /*
-        data2.addRows([
-            [new Date(dt[0]), a[0], "semmosta"],
-            [new Date(dt[1]), a[1], "tommosta"],
-            [new Date(dt[2]), a[2], "oho"]
 
-        ]);
-*/
         chart.draw(data2, {
             displayAnnotations: true,
         });
-
+        chart.setVisibleChartRange(null, null);
 
     });
 }
